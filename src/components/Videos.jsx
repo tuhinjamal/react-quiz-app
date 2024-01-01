33562e711b06a635e9e8b714b964d46ec41435f3
tuhinjamal <tuhinjamal1@egmail.com>
2024-01-01 18:22:11 +0600

    fixed

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -6,7 +6,7 @@ export default function Videos() {
   const { loading, error, videos } = useVideoList();
   return (
     <>
-      <div className={classes.videos}>
+      {/* <div className={classes.videos}>
         {videos.length > 0 &&
           videos.map((video) => {
             <Link to="/quiz" key={video.youtubeID}>
@@ -15,6 +15,21 @@ export default function Videos() {
           })}
         {!loading && videos.length === 0 && <div>No Data Found</div>}
         {error && <div>There was an error</div>}
+        {loading && <div>Loading...</div>}
+      </div> */}
+      <div className={classes.videos}>
+        {videos.map((video) =>
+          video.noq > 0 ? (
+            <Link to="/quiz" key={video.youtubeID}>
+              <Video title={video.title} id={video.youtubeID} noq={video.noq} />
+            </Link>
+          ) : (
+            ""
+          )
+        )}
+
+        {!loading && videos.length === 0 && <div>No data found!</div>}
+        {error && <div>There was an error!</div>}
         {loading && <div>Loading...</div>}
       </div>
     </>
--- src/hooks/useVideoList.jsx
+++ src/hooks/useVideoList.jsx
@@ -1,35 +1,53 @@
+import {
+  get,
+  getDatabase,
+  limitToFirst,
+  orderByKey,
+  query,
+  ref,
+  startAt,
+} from "firebase/database";
 import { useEffect, useState } from "react";
-import { getDatabase, ref, query, orderByKey, get } from "firebase/database";
-export default function useVideoList() {
+
+export default function useVideoList(page) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [videos, setVideos] = useState([]);
+  //   const [hasMore, setHasMore] = useState(true);
+
   useEffect(() => {
-    // database  related work
     async function fetchVideos() {
+      // database related works
       const db = getDatabase();
-      const videoRef = ref(db, "videos");
-      const videoQuery = query(videoRef, orderByKey());
+      const videosRef = ref(db, "videos");
+      const videoQuery = query(videosRef, orderByKey());
+
       try {
         setError(false);
         setLoading(true);
-        //   request firebase database
+        // request firebase database
         const snapshot = await get(videoQuery);
+        setLoading(false);
         if (snapshot.exists()) {
           setVideos((prevVideos) => {
-            return [...prevVideos, ...Object.values(snapshot.val)];
+            return [...prevVideos, ...Object.values(snapshot.val())];
           });
         } else {
-          //
+          //   setHasMore(false);
         }
-        setLoading(false);
       } catch (err) {
         console.log(err);
         setLoading(false);
         setError(true);
       }
     }
+
     fetchVideos();
   }, []);
-  return { loading, error, videos };
+
+  return {
+    loading,
+    error,
+    videos,
+  };
 }
